e6772623611f4559ad477f2708a10b56d252aa36
k1ngjet3r <jeter.lin1208@gmail.com>
2021-03-31 12:42:22 +0800

    0331 usage
    adding ctrl.py

--- case_list_generator.py
+++ case_list_generator.py
@@ -1,7 +1,7 @@
 from openpyxl import load_workbook, Workbook
 
 # Loading the full-list file
-full_list = load_workbook('W13_sorted.xlsx')['auto']
+full_list = load_workbook('W14_sorted.xlsx')['auto']
 
 # Creating the automation-related cases spreadsheet
 wb = Workbook()
